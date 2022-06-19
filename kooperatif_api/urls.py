83c60baa1c5523867385861da23a4c4c61fe446b
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2022-06-19 11:45:19 +0300

    create backend for cooperative

--- kooperatif_api/urls.py
+++ kooperatif_api/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path, include
+from django.urls import path
+
 from .views import *
 
-
 urlpatterns = [
-    path('payers', payers),
-    path('payer/<payer_id>', get_single_payer),
-    path('pay-records', get_pay_records),
-    path('pay-record/<payer_id>', get_single_pay_record),
+    # API paths
+    path('api/payers', get_payers),
+    path('api/payer/<payer_id>', get_single_payer),
+    path('api/pay-records', get_pay_records),
+    path('api/pay-record/<payer_id>', get_single_pay_record),
+
+    # TEMPLATE paths
+    path('payers', payers_page),
+    path('pay-records', pay_records_page),
 ]
--- kooperatif_api/views.py
+++ kooperatif_api/views.py
@@ -1,14 +1,15 @@
+import time
+
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from .serializers import *
 
 
 @api_view(['GET', 'POST'])
-def payers(request):
+def get_payers(request):
     if request.method == 'GET':
-        # context = {"payers": Payer.objects.all()}
-        # return render(request, 'payers.html', context=context)
         all_payers = Payer.objects.all()
         serializer = PayerSerializer(all_payers, many=True)
         return Response(serializer.data)
@@ -17,15 +18,34 @@ def payers(request):
 @api_view(['GET'])
 def get_single_payer(request, payer_id):
     get_payer = Payer.objects.get(pk=payer_id)
+    pay_records = PayRecord.objects.filter(payer=get_payer)
+
+    pay_records_serializer = PayRecordSerializer(pay_records, many=True)
     serializer = PayerSerializer(get_payer)
-    return Response(serializer.data)
+
+    result = serializer.data
+    result['pay-records'] = pay_records_serializer.data
+    return Response(result)
 
 
 @api_view(['GET', 'POST'])
 def get_pay_records(request):
     if request.method == "GET":
-        pay_records = PayRecord.objects.all()
+        date = request.GET.get('date', '')
+        pay_records = []
+        if date != '':
+            date_datetime = time.strptime(date, '%Y-%m-%d')
+            pay_records = PayRecord.objects.filter(pay_date__year=date_datetime.tm_year,
+                                                   pay_date__month=date_datetime.tm_mon,
+                                                   pay_date__day=date_datetime.tm_mday)
+        else:
+            pay_records = PayRecord.objects.all()
+
         serializer = PayRecordSerializer(pay_records, many=True)
+
+        for i, pay_record in enumerate(pay_records):
+            serializer.data[i]['payer-info'] = PayerSerializer(pay_record.payer).data
+
         return Response(serializer.data)
 
 
@@ -40,3 +60,19 @@ def get_single_pay_record(request, payer_id):
         serializer.data[i]['payer-info'] = payer_serializer.data
 
     return Response(serializer.data)
+
+
+def payers_page(request):
+    payers = Payer.objects.all()
+    context = {
+        'payers': payers
+    }
+    return render(request, 'payers.html', context=context)
+
+
+def pay_records_page(request):
+    pay_records = PayRecord.objects.all().order_by("pay_date")
+    context = {
+        'pay_records': pay_records
+    }
+    return render(request, 'pay-records.html', context=context)
